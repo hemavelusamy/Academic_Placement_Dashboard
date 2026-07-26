33270629e605893456b624ddc082b3aa28bfe043
Hemalatha <hemavelusamy9@gmail.com>
2026-07-26 13:33:39 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-95@b2sk86!gs4y%!qc0eg+j_%lz@y7gtj2z0m6bb0+@4&%!x9%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
